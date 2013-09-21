6ad64d1bf676b76b3f97a402b1844c6e2f6406f6
Tekhne <tekhneios@onixpro.local>
2013-09-21 11:50:18 -0400

    Initial Commit

---
+++ PiedraPapelTijera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PiedraPapelTijera
+//
+//  Created by Tekhne on 9/21/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PiedraPapelTijeraTests/PiedraPapelTijeraTests.m
@@ -0,0 +1,34 @@
+//
+//  PiedraPapelTijeraTests.m
+//  PiedraPapelTijeraTests
+//
+//  Created by Tekhne on 9/21/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PiedraPapelTijeraTests : XCTestCase
+
+@end
+
+@implementation PiedraPapelTijeraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
